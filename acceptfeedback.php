37a062faa6bdecd2f34a76ce5f4a732f51df5343
crazyeight68@gmail.com <Cararoo68>
2017-03-23 23:24:04 -0700

    Adding additional styling, as well as a test page to receive that of userfeedback.php.

---
+++ acceptfeedback.php
@@ -0,0 +1,13 @@
+<?php include "inc/header.php";?>
+<div class="content">
+<div id="wrapper" class="content-inside">
+    <h2>THANK YOU FOR SENDING DATA!</h2><br>
+    <p>Your first name: <?php echo $_POST['feed-fname'];?></p><br>
+    <p>Your last name: <?php echo $_POST['feed-lname'];?></p><br>
+    <p>Your email: <?php echo $_POST['feed-email'];?></p><br>
+</div>
+</div>
+
+
+
+<?php include "inc/footer.php";?>
--- userfeedback.php
+++ userfeedback.php
@@ -4,10 +4,12 @@ include "inc/header.php";
 include "inc/linkstyler.php";
 ?>
 
-<h2>HERE THE USER CAN SUBMIT FEEDBACK TICKETS</h2>
 <div class="content">
 <div class="content-inside" id="wrapper">
-<form action="" method="post">
+
+<h2>HERE THE USER CAN SUBMIT FEEDBACK TICKETS</h2>
+
+<form action="acceptfeedback.php" method="post">
 
   <label for="fname">First Name:</label>  <input type="text" id="fname" name="feed-fname"><br>
 
@@ -15,7 +17,7 @@ include "inc/linkstyler.php";
 
   <label for="email">Email:</label><input type="text" id="email" name="feed-email"><br>
 
-  <label for="comm">Comment:</label><textarea rows="15" cols="80" name="comment">Enter text here...</textarea><br>
+  <label for="comm">Comment:</label><textarea rows="15" cols="80" name="comment" placeholder="Enter text here..."></textarea><br>
 
   <input type="submit" value="Submit">
  </form>
@@ -25,7 +27,7 @@ include "inc/linkstyler.php";
 
 
 
-
+<div>
 
 
 
@@ -36,7 +38,5 @@ include "inc/linkstyler.php";
 
 
 <?php
-    echo var_dump($_POST['feed-fname']);
-
-    include "inc/footer.php"
+    include "inc/footer.php";
  ?>
